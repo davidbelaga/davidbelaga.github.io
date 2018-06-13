a9501911bdc70565993300b8fbfb1a24858f983c
davidbelaga <davidbelaga@hotmail.com>
2018-06-13 14:53:21 +0200

    pop-up essai 2

--- bettermfw.js
+++ bettermfw.js
@@ -1,4 +1,6 @@
-function popupBasique(page) {
-  window.open(page);
+function popup(mylink, windowname) { 
+    if (! window.focus)return true; var href; if (typeof(mylink) == 'string') href=mylink; else href=mylink.href; window.open(href, windowname, 'width=400,height=200,scrollbars=yes'); return false; 
 }
+
+window.onload = function(){popup('autopopup.html', 'thp')};
 
