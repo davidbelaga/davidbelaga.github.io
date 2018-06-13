6e9235be6d1b451676de029b12acbb5e610e8335
davidbelaga <davidbelaga@hotmail.com>
2018-06-13 18:41:06 +0100

    modification de bettermfw.js

--- bettermfw.js
+++ bettermfw.js
@@ -10,3 +10,42 @@ window.onload = function(){popup('autopopup.html', 'thp')};
 
 setTimeout(function() { alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"); }, 10000);
 
+
+// mode psychédélique
+function doThatFuckingColorThing(){
+  var color = 'hsl(' + currentHue + ',80%, 60%)';
+  if(currentHue + hueAddition > 360)
+  {
+    currentHue = 0;
+  }
+  else {
+    currentHue += hueAddition;
+  }
+  r.style.color = color;
+  setTimeout(function(){doThatFuckingColorThing();}, 1000/25);
+}
+
+function someControl(id, textArr, className){
+  // You see? No fucking jQuery needed, check http://jsperf.com/getelementbyid-vs-jquery-id/44
+  var acbox = document.getElementById(id);
+  var clickEvent = ('ontouchstart' in window ? 'touchend' : 'click');
+  var el = document.getElementsByTagName('html')[0];
+  acbox.addEventListener(clickEvent, function(){
+    if(acbox.innerHTML === textArr[0]){
+      el.classList.add(className);
+      acbox.innerHTML = textArr[1];
+    }
+    else{
+      el.classList.remove(className);
+      acbox.innerHTML = textArr[0];
+    }
+  });
+}
+
+
+function addPsychColorControl(){
+  someControl('psychmode', ['Mode Psychédélique', 'Normal mode'], 'psychedelic');
+}
+
+addContrastControl();
+addInvertedControl();
