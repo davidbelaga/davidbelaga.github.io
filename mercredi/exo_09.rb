7caf52bd3667f8faf0876cb49b63320e0f1a9f70
davidbelaga <davidbelaga@hotmail.com>
2018-06-21 10:49:04 +0200

    dernière version qui fonctionne, yeahgit add .! du mode psychédélique

---
+++ mercredi/exo_09.rb
@@ -0,0 +1,7 @@
+puts "C'est quoi ton prénom?"
+print ">"
+user_first_name = gets.chomp
+puts "C'est quoi ton nom?"
+print ">"
+user_last_name = gets.chomp
+puts "Bonjour, " + user_first_name + " " + user_last_name + " !"
